5d4784973ce194dac48c2236145cb09114d350cf
joelchan <joelchanzhiyang@gmail.com>
2023-05-11 10:05:14 +0800

    Remove unused /upload endpoint

--- app.py
+++ app.py
@@ -50,29 +50,6 @@ def invocations():
     return jsonify(img=result_image_data, stat=stats)
 
 
-@app.route('/upload', methods=['POST'])
-def uploadFile():
-    """
-    REST API endpoint for image inference
-
-    Returns:
-        JSON Dictionary -- Return a dictionary containing image in base64 format and statistics of the counts and classification of rotifers
-    """
-    file = request.files['file']
-    auto_crop = int(request.form['auto_crop'])
-    file.save("./test.jpg")
-
-    if auto_crop == 1:
-        autocrop.autocrop("./test.jpg")
-
-    stats = darknet.performDetect(imagePath="./test.jpg", configPath="./yolo-obj.cfg",
-                                  weightPath="./backup/yolo-obj_best.weights", metaPath="./data/obj.data")
-    with open("result.jpg", "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        base64_string = encoded_string.decode('utf-8')
-    return jsonify(img=base64_string, stat=stats)
-
-
 if __name__ == '__main__':
     # predict()
     app.run(host='0.0.0.0', port=8080)
